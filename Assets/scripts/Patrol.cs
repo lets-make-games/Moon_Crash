36f49dd1aebb5c0f11c2d955db854de5a47c13ce
Soheilk47 <soheil47g@gmail.com>
2022-04-06 20:04:05 +0430

    fixed the patrol script
    it was made for 2d platformer not a top down .

--- Assets/scripts/Patrol.cs
+++ Assets/scripts/Patrol.cs
@@ -15,18 +15,17 @@ public class Patrol : MonoBehaviour
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
-        if (groundInfo.collider == false)
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.right, distance);
+        if (groundInfo.collider == true)
         {
+            movingRight = !movingRight;
             if (movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
             }
             else
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
             }
         }
     }
