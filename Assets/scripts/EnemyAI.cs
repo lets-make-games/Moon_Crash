885aa853059941995a5f39ef652362462bab5c26
marckes04 <maresp95@gmail.com>
2022-04-08 11:37:25 -0500

    AI Enemy was added

---
+++ Assets/scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float Speed = 0f;
+    private Transform target;
+    public GameObject tar;
+    public float Stopping_Distance;
+
+
+    void Start()
+    {
+        target = tar.GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,target.position) < Stopping_Distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+        }
+    }
+}
