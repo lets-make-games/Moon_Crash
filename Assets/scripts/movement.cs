64dbb14a36cc8d5e22ff7235d5fcca4256354a63
Soheilk47 <soheil47g@gmail.com>
2022-04-08 19:37:09 +0430

    character animation

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -9,34 +9,74 @@ public class movement : MonoBehaviour
 
     public LayerMask WhatIsSolid;
 
-
+    private Animator animator;
     private void Start()
     {
         movePoint.parent = null;
-
+        animator = GetComponent<Animator>();
     }
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
-        Vector3 HorozontalInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-        Vector3 VerticalInput = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime); //move to point
+
+        Vector3 HorozontalVec = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f); //assigning values
+        Vector3 VerticalVec = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        float HorInput = Input.GetAxisRaw("Horizontal");
+        float VerInput = Input.GetAxisRaw("Vertical");
+
 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) 
+            animator.SetFloat("Speed", Mathf.Abs(HorInput + VerInput));  //set hor and ver parameters
+
+
+            if (HorInput == 0f)  //idle
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + HorozontalInput, 0.2f, WhatIsSolid))
+                animator.SetFloat("Horizontal", 0f);
+            }
+            else if (VerInput == 0f)  //idle
+            {
+                animator.SetFloat("Vertical", 0f);
+            }
+
+
+            if (HorInput == 1f)  //moving right
+            {
+
+                if (!Physics2D.OverlapCircle(movePoint.position + HorozontalVec, 0.2f, WhatIsSolid))
                 {
-                    movePoint.position += HorozontalInput;
+                    movePoint.position += HorozontalVec;
+                    animator.SetFloat("Horizontal", 1f);
                 }
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            else if (HorInput == -1f)  //moving left
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + VerticalInput, 0.2f, WhatIsSolid))
+
+                if (!Physics2D.OverlapCircle(movePoint.position + HorozontalVec, 0.2f, WhatIsSolid))
                 {
-                    movePoint.position += VerticalInput;
+                    movePoint.position += HorozontalVec;
+                    animator.SetFloat("Horizontal", -1f);
                 }
             }
+
+
+            else if (VerInput == 1f)  //moving up
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + VerticalVec, 0.2f, WhatIsSolid))
+                {
+                    movePoint.position += VerticalVec;
+                    animator.SetFloat("Vertical", 1f);
+                }
+            }
+            else if (VerInput == -1f)  //moving down
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + VerticalVec, 0.2f, WhatIsSolid))
+                {
+                    movePoint.position += VerticalVec;
+                    animator.SetFloat("Vertical", -1f);
+                }
+            }
+
         }
     }
 }
