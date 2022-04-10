c047bbb04abbcfdd3c7ed58a9451022a4f8eac4f
Soheilk47 <soheil47g@gmail.com>
2022-04-10 21:10:21 +0430

    teleport shake

--- Assets/scripts/teleport.cs
+++ Assets/scripts/teleport.cs
@@ -8,29 +8,37 @@ public class teleport : MonoBehaviour
     private GameObject movepoint;
     private AudioSource teleportSound;
 
+    private Animator camAnim;
     private void Start()
     {
         teleportSound = GetComponent<AudioSource>();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D colli)
     {
         movepoint = GameObject.FindGameObjectWithTag("movePoint");  //define movepoint
 
         movepoint.transform.position = gameObject.GetComponent<Renderer>().bounds.center;
-        collision.transform.position = gameObject.GetComponent<Renderer>().bounds.center;
+        colli.transform.position = gameObject.GetComponent<Renderer>().bounds.center;
 
-        collision.GetComponent<movement>().enabled = false;  //disable movement
+        colli.GetComponent<movement>().enabled = false;  //disable movement
 
         StartCoroutine(time());
 
         IEnumerator time()
         {
+            camAnim = colli.GetComponent<Animator>(); //cam shake
+            camAnim.SetBool("teleporting", true);
+
             teleportSound.Play(0);  //sound
             yield return new WaitForSeconds(3);  //timer
 
             movepoint.transform.position = teleportTo.transform.position;  //move the point
-            collision.transform.position = teleportTo.transform.position;  //move the player
-            collision.GetComponent<movement>().enabled = true; //enable movement
+            colli.transform.position = teleportTo.transform.position;  //move the player
+            colli.GetComponent<movement>().enabled = true; //enable movement
+
+            camAnim.SetBool("teleporting", false);
         }
     }
+
+
 }
